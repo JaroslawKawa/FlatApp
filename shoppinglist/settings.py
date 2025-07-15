c0456d8b023b13ae1686b6fdbd2a10791e702c17
Jaroslaw Kawa <jaroslaw.kawa@aptiv.com>
2025-07-15 10:21:50 +0200

    new settings.py for render.com

--- shoppinglist/settings.py
+++ shoppinglist/settings.py
@@ -52,17 +52,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'shoppinglist.wsgi.application'
 
-if os.environ.get('DATABASE_URL'):
-    DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
 
 AUTH_PASSWORD_VALIDATORS = []
 
